c7cad5beb525304dafca93d44f1c9cd0dbaa8233
Nongeoking <kbsowo14@naver.com>
2025-10-14 11:02:45 +0900

    feat: on

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button, ButtonProps } from './lib/components/Button';
-export { default as Input, InputProps } from './lib/components/Input';
-export { default as Card, CardProps } from './lib/components/Card';
-
--- index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Button } from './lib/components/Button';
-export { default as Input } from './lib/components/Input';
-export { default as Card } from './lib/components/Card';
-
-export type { ButtonProps } from './lib/components/Button';
-export type { InputProps } from './lib/components/Input';
-export type { CardProps } from './lib/components/Card';
-
